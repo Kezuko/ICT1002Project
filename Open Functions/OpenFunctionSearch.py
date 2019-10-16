702762ae030cc9bfe2f142a921f9fc69d079c1e2
benjaminkoh1994 <55873746+benjaminkoh1994@users.noreply.github.com>
2019-10-16 22:55:15 +0800

    Update OpenFunctionSearch.py

--- Open Functions/OpenFunctionSearch.py
+++ Open Functions/OpenFunctionSearch.py
@@ -1,6 +1,5 @@
 import csv
 
-
 def searchTenderIdNo(idNo, csv_file):
     tenderNo = []
     agency = []
@@ -44,22 +43,38 @@ def searchTenderIdNo(idNo, csv_file):
 
 
 def searchTenderIdCode(idCode, csv_file):
-    myList = []
+    results = []
+    tenderNo = []
+    agency = []
+    tenderDescription = []
+    awardDate = []
+    tenderDetailStatus = []
+    supplierName = []
+    awardedAmt = []
+
     f = open(csv_file, 'rb')
     reader = csv.reader(f, delimiter=',')
     header = False
 
     for row in reader:
         if idCode in row[0]:
-            myList.append(row)
+            results.append(row)
+            tenderNo.append(str(row[0]))
+            agency.append(str(row[1]))
+            tenderDescription.append(str(row[2]))
+            awardDate.append(str(row[3]))
+            tenderDetailStatus.append(str(row[4]))
+            supplierName.append(str(row[5]))
+            awardedAmt.append(str(row[6]))
 
     else:
         header = True
 
     f.close()
 
-    return myList
+    print results
 
 
-print searchTenderIdNo("AGO000ETT15000001", "government-procurement-via-gebiz.csv")
-print searchTenderIdCode("AVA", "government-procurement-via-gebiz.csv")
+# print searchTenderIdCode("AGO", "government-procurement-via-gebiz.csv")
+# print searchTenderIdNo("AGO000ETT15000001", "government-procurement-via-gebiz.csv")
+
