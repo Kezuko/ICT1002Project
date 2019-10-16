2c61e0ec2baf5b5d1375eb89b5707437805d5103
benjaminkoh1994 <55873746+benjaminkoh1994@users.noreply.github.com>
2019-10-16 21:02:54 +0800

    Update Function3.py

--- Function3.py
+++ Function3.py
@@ -84,49 +84,6 @@ def eachSectorTotalAmtProcurementDsc(csv_file):
     return recordDsc
 
 
-def searchTenderIdNo(uniqueNo, csv_file):
-    tenderNo = []
-    agency = []
-    tenderDescription = []
-    awardDate = []
-    tenderDetailStatus = []
-    supplierName = []
-    awardedAmt = []
-
-    f = open(csv_file, 'rb')
-    reader = csv.reader(f, delimiter=',')
-    found = False
-
-    for row in reader:
-        tender = row[0]
-        if tender == uniqueNo:
-            tenderNo.append(str(row[0]))
-            agency.append(str(row[1]))
-            tenderDescription.append(str(row[2]))
-            awardDate.append(str(row[3]))
-            tenderDetailStatus.append(str(row[4]))
-            supplierName.append(str(row[5]))
-            awardedAmt.append(str(row[6]))
-            break  # leave for loop
-    else:
-        found = True
-
-    f.close()
-
-    searchReturn = {
-        "Tender No.": tenderNo,
-        "Agency": agency,
-        "Tender Description": tenderDescription,
-        "Award Date": awardDate,
-        "Tender Detail Status": tenderDetailStatus,
-        "Supplier Name": supplierName,
-        "Awarded Amount": awardedAmt
-    }
-
-    return searchReturn
-
-
-print searchTenderIdNo("AGC000ETT14000010", "government-procurement-via-gebiz.csv") #test print search by ID
 print function3("government-procurement-via-gebiz.csv") #test print default list
 print function3Asc("government-procurement-via-gebiz.csv") #test print ascending list
 print function3Dsc("government-procurement-via-gebiz.csv") #test print descending list
