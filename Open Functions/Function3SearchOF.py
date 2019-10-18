4d09aa89f63a4b735c5e36f28b7649cf33fd2ab4
benjaminkoh1994 <55873746+benjaminkoh1994@users.noreply.github.com>
2019-10-19 03:13:23 +0800

    Update Function3SearchOF.py

--- Open Functions/Function3SearchOF.py
+++ Open Functions/Function3SearchOF.py
@@ -53,27 +53,38 @@ def searchTenderIdCode(idCode):
     df = pd.read_csv('government-procurement-via-gebiz.csv')
     dfList = df.loc[:, ['tender_no']]
 
-    # Determine and locate all rows that contains value defined by user
+    # Determine and  locate all rows that contains value defined by user
     results = df.loc[dfList.tender_no.str.contains(idCode)]
-
-    return results
+    # results.index = [x for x in range(1, len(results.values) + 1)]
+    # results.index.name = 'id_no'
+    # Add an unique Id number column and assign to each row
+    results = results.assign(id_no=[i for i in xrange(len(results))])[['id_no'] + results.columns.tolist()]
+    
+    # Return results with index set and as a dictionary
+    myDR = results.set_index('id_no').T.to_dict('list')
+    return myDR
 
 
 def searchDateRange(startDate, endDate):
     # Opens the CSV file and locate the award_date column
     df = pd.read_csv('government-procurement-via-gebiz.csv', parse_dates=True)
     dfList = df.loc[:, ['award_date']]
-    
+
     # Convert the starting date and ending date into a datetime format
     dfList.award_date = pd.to_datetime(dfList.award_date)
-    
-    # Determine the values between two dates of a date column as user defined
+
+    # Determine the values between two dates of a date column as per user defined
     results = df.loc[(dfList.award_date > startDate) & (dfList.award_date <= endDate)]
+    
+    # Add an unique Id number column and assign to each row
+    results = results.assign(id_no=[i for i in xrange(len(results))])[['id_no'] + results.columns.tolist()]
+    
+    # Return results with index set and as a dictionary
+    myDF = results.set_index('id_no').T.to_dict('list')
+    return myDF
 
-    return results
 
-
-# print searchDateRange("2015-2-5", "2015-2-7")
+# print searchDateRange("2015-02-04", "2015-02-06")
 # print searchTenderIdCode("AGC")
 # print searchTenderIdNo("AGO000ETT15000001", "government-procurement-via-gebiz.csv")
 
