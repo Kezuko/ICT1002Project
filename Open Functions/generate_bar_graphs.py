9416401709f5b52d9548fd16c8e61428f472acfd
Kezuko <Kezukoxcellence@gmail.com>
2019-10-19 21:58:51 +0800

    Fixed spacing

--- Open Functions/generate_bar_graphs.py
+++ Open Functions/generate_bar_graphs.py
@@ -17,7 +17,6 @@ def generate_bar_graphs(csv_file1='government-procurement-via-gebiz.csv', csv_fi
     records = generate_all_spending_records()
     TOP = 10 #change this integer to change top amount of spending
     
-    
     if choice == "1":
         x = generate_sb_list()
         y = []
@@ -25,10 +24,8 @@ def generate_bar_graphs(csv_file1='government-procurement-via-gebiz.csv', csv_fi
         for i in records:
             if i in x:
                 y.append(records[i])
-
         for i in range(len(x)):
             d[x[i]] = y[i]
-
         sorted_d = sorted(d.items(), key=operator.itemgetter(1))[::-1] #descending order
         x = []
         y = []
@@ -36,24 +33,13 @@ def generate_bar_graphs(csv_file1='government-procurement-via-gebiz.csv', csv_fi
         for i in sorted_d:
             x.append(i[0])
             y.append(i[1])
-                    
-
-        
         data = {'x':x, 'Award Amount':y}
         df = pd.DataFrame(data)
         df = df.head(TOP)
-        
         df.plot.barh(x='x', y='Award Amount', colormap='Paired')
         plt.title('Top {0} SB Award Amount'.format(TOP))
-        
-        
         plt.show()
         
-        
-        
-
-
-
     elif choice == "2":
         x = generate_dd_list()
         y = []
@@ -61,34 +47,20 @@ def generate_bar_graphs(csv_file1='government-procurement-via-gebiz.csv', csv_fi
         for i in records:
             if i in x:
                 y.append(records[i])
-
         for i in range(len(x)):
             d[x[i]] = y[i]
-
         sorted_d = sorted(d.items(), key=operator.itemgetter(1))[::-1] #descending order
         x = []
         y = []
-        
         for i in sorted_d:
             x.append(i[0])
             y.append(i[1])
-
-            
-        
-
-        
         data = {'x':x, 'Award Amount':y}
         df = pd.DataFrame(data)
         df = df.head(TOP)
-        
         df.plot.barh(x='x', y='Award Amount', colormap='Paired')
         plt.title('Top {0} DD Award Amount'.format(TOP))
-        
-        
         plt.show()
-        
-
-
     elif choice == "3":
         x = generate_sb_list() + generate_dd_list()
         y = []
@@ -96,30 +68,19 @@ def generate_bar_graphs(csv_file1='government-procurement-via-gebiz.csv', csv_fi
         for i in records:
             if i in x:
                 y.append(records[i])
-
         for i in range(len(x)):
             d[x[i]] = y[i]
-
         sorted_d = sorted(d.items(), key=operator.itemgetter(1))[::-1] #descending order
         x = []
         y = []
-
         for i in sorted_d:
             x.append(i[0])
             y.append(i[1])
-
-            
-        
-
-        
         data = {'x':x, 'Award Amount':y}
         df = pd.DataFrame(data)
         df = df.head(TOP)
-        
         df.plot.barh(x='x', y='Award Amount', colormap='Paired')
         plt.title('Top {0} DD/SB Award Amount'.format(TOP))
-        
-        
         plt.show()
               
 
