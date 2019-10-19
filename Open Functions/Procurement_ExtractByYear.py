c21eedcf1e325963289f4d9e8e655c2e2b443967
linghu258 <ranger0504@hotmail.com>
2019-10-19 21:43:14 +0800

    Update Procurement_ExtractByYear.py

--- Open Functions/Procurement_ExtractByYear.py
+++ Open Functions/Procurement_ExtractByYear.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import collections
+
 
 governmentcsv = pd.read_csv('government-procurement-via-gebiz.csv',parse_dates=True) #read csv and it contain dates (parse_dates = true)
 extract = governmentcsv.loc[:, ['supplier_name','award_date']] #only getting these columns
@@ -15,27 +17,24 @@ def extract_supplier_not_na():
 
 # # #supplier not na and year 2015
 def extract_supplier_not_na_2015():
-    notNAFifteen = extract[(extract.supplier_name != 'na') & (extract.award_date.dt.year == 2015)] #extract only year 2016
-    notNAFifteen.reset_index(drop = True,inplace=True) #reset index
-    notNAFifteen.index += 1 #and index start from 1
-    #SortednotNAFifteen = collections.orderedDictionary(notNAFifteen)
+    notNAFifteen = dict(zip(extract['supplier_name'], extract.loc[extract.award_date.dt.year == 2015, 'award_date']))
+
     return notNAFifteen
-print extract_supplier_not_na_2015()
+#print extract_supplier_not_na_2015()
+
 
 # # #supplier not na and year 2016
 def extract_supplier_not_na_2016():
-    notNASixteen = extract[(extract.supplier_name != 'na') & (extract.award_date.dt.year == 2016)] #extract only year 2016
-    notNASixteen.reset_index(drop = True,inplace=True) #reset index
-    notNASixteen.index += 1 #and index start from 1
+    notNASixteen = dict(zip(extract['supplier_name'], extract.loc[extract.award_date.dt.year == 2016, 'award_date']))
+
     return notNASixteen
 #print extract_supplier_not_na_2016()
 
 #
 # # #supplier not na and year 2017
 def extract_supplier_not_na_2017():
-    notNASeventeen = extract[(extract.supplier_name != 'na') & (extract.award_date.dt.year == 2017)] #extract only year 2016
-    notNASeventeen.reset_index(drop = True,inplace=True) #reset index
-    notNASeventeen.index += 1 #and index start from 1
+    notNASeventeen = dict(zip(extract['supplier_name'], extract.loc[extract.award_date.dt.year == 2017, 'award_date']))
+
     return notNASeventeen
 #print extract_supplier_not_na_2017()
 
