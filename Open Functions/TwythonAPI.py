235789c9dbd15cbf8b242d2eba4f4506764b19de
Kezuko <Kezukoxcellence@gmail.com>
2019-10-19 16:36:22 +0800

    Changes to make it function

--- Open Functions/TwythonAPI.py
+++ Open Functions/TwythonAPI.py
@@ -1,13 +1,16 @@
 import json
 from twython import Twython
 
-#Personal API key
-twitter = Twython("DubGZhfy08e2kF7r4Z1FnH5nu", "hfX9xdxvppgMIKaeJi2X8fzbood46UNyRJa25yzTQr931OOAmN",
-                  "285684564-QgwQAfaSgFLwlWNzl6JIpD6WlDCYPyiZsGKfU4Uc", "Lcx2SpsfGoKaxrtqg3sZOnIHIntfYZJpBMgUkPZGAXoyI")
+def twitterAPI():
+    #Personal API key
+    twitter = Twython("DubGZhfy08e2kF7r4Z1FnH5nu", "hfX9xdxvppgMIKaeJi2X8fzbood46UNyRJa25yzTQr931OOAmN",
+                      "285684564-QgwQAfaSgFLwlWNzl6JIpD6WlDCYPyiZsGKfU4Uc", "Lcx2SpsfGoKaxrtqg3sZOnIHIntfYZJpBMgUkPZGAXoyI")
 
-#Search for either the word "procurement" or "#procurement"
-results = twitter.search(q='procurement OR #procurement', result_type='popular')
+    #Search for either the word "procurement" or "#procurement"
+    results = twitter.search(q='procurement OR #procurement', result_type='popular')
 
-#save the JSON output into a txt file for future usage
-with open("Twitter.txt", "w") as text_file:
-    json.dump(results, text_file)
+    #save the JSON output into a txt file for future usage
+    with open("Twitter.txt", "w") as text_file:
+        json.dump(results, text_file)
+
+#twitterAPI()
