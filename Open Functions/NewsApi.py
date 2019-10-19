9cca9f6ae1f54096b03771b7920c08670de97728
Kezuko <Kezukoxcellence@gmail.com>
2019-10-19 16:35:55 +0800

    Changes it to Function

--- Open Functions/NewsApi.py
+++ Open Functions/NewsApi.py
@@ -1,17 +1,20 @@
 import requests
 import json
 
-url = ('https://newsapi.org/v2/everything?' #API URL
-       'q=procurement AND tender&' #keywords on procurement AND tender
-       'sortBy=popularity&' #Sort them by popularity
-       'apiKey=365fe4d44dba4aa98a00a7497200e6f9') #Personal API key
+def newsAPI():
+    url = ('https://newsapi.org/v2/everything?' #API URL
+           'q=procurement AND tender&' #keywords on procurement AND tender
+           'sortBy=popularity&' #Sort them by popularity
+           'apiKey=365fe4d44dba4aa98a00a7497200e6f9') #Personal API key
 
-# GET
-response = requests.get(url)
+    # GET
+    response = requests.get(url)
 
-#storing the output into variable "results"
-results = response.json()
+    #storing the output into variable "results"
+    results = response.json()
 
-# save the JSON output into a txt file for future usage
-with open("NewsAPI.txt", "w") as text_file:
-    json.dump(results, text_file)
+    # save the JSON output into a txt file for future usage
+    with open("NewsAPI.txt", "w") as text_file:
+        json.dump(results, text_file)
+
+#newsAPI()
